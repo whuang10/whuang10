14deaf56579bb73a5e9f5fa807513be79cbbce83
whuang10 <71650624+whuang10@users.noreply.github.com>
2020-09-29 20:58:52 -0400

    Update krewes.py

--- 05/krewes.py
+++ 05/krewes.py
@@ -1,9 +1,7 @@
 # Winnie Huang (with Liam Kronman and Matthew Hui)
 # SoftDev
-# K05 -- Teamwork, but Better This Time; Printing the name of a randomly-slected student from team (orpheus, rex, or endymion)
+# K05 -- Teamwork, but Better This Time; Printing the name of a randomly-selected student from team (orpheus, rex, or endymion)
 # 2020-09-29
-
-
 
 import random
 
@@ -13,10 +11,13 @@ KREWES = {
     'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
 }
 
+# user inputs team name
 team = input ("Please enter a team you would like to target: ")
 
+# troubleshoots if user inputs invalid team name
 while team not in KREWES.keys():
     team = input("Please re-enter a valid team name: ")
 
+# prints random student name from inputted team name
 print (random.choice(KREWES[team]))
 
